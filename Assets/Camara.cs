49f5ddef8b3b875627862c0f3510e034618ba05f
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-09-01 11:38:52 -0500

    Cambios iniciales

---
+++ Assets/Camara.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform Cube;
+    public Vector3 offset;
+
+    void Update()
+    {
+        if(Cube != null){
+            transform.position = Cube.position+offset;
+        }
+    }
+}
