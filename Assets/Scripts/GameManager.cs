8a7a6da8305233d1ca3fc43c82ff88677ef1e299
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-09-29 12:42:58 -0500

    menu/contador

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static bool activado = false;
-    public static string GameState = "none";
+    public static bool GameState = false;
     [SerializeField] GameObject play;
     [SerializeField] GameObject ingame;
+
+    public TextMeshProUGUI texto;
+    private int contador = 0; 
+
     void Start()
     {
         ingame.SetActive(false);
+        texto = GetComponent<TextMeshProUGUI>();
+        ActualizarContador();
     }
 
     void Update()
@@ -18,11 +25,17 @@ public class GameManager : MonoBehaviour
         
     }
 
+    private void ActualizarContador(){
+        contador++;
+        string contadorConCeros = contador.ToString("D5");
+        texto.text = contadorConCeros;
+    }
+
     public void activarig(){
-    play.SetActive(false);
-    ingame.SetActive(true);
-    GameManager.activado = true;
-    GameManager.GameState= "play";
+        play.SetActive(false);
+        ingame.SetActive(true);
+        GameManager.activado = true;
+        GameManager.GameState= true;
     }
 }
 
