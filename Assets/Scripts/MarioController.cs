ec55a1c9100f33a9ff84a94a4dfa3322f7f6799b
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-10-06 11:46:51 -0500

    colisiones

--- Assets/Scripts/MarioController.cs
+++ Assets/Scripts/MarioController.cs
@@ -1,22 +1,59 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MarioController : MonoBehaviour
 {
-    private bool isBigMario = false; // Variable para rastrear si Mario es grande o no.
-    public float bigMarioScale = 2f; // Tamaño de Mario cuando es grande.
+    private bool isBigMario = false;
+    private Dictionary<GameObject, int> collisionsWithCubes = new Dictionary<GameObject, int>();
+    public float bigMarioScaleY = 2f;
+    public UIManager ui;
+    public Transform raycastOrigin; // Debe ser una Transform en la parte superior del jugador.
 
-    private void OnCollisionEnter(Collision objeto)
+    private void Update()
     {
-        if (objeto.gameObject.CompareTag("Hongo")) // Asegúrate de que el hongo tenga el tag "Hongo".
+        // Lanza un Raycast hacia abajo desde el punto superior del jugador.
+        RaycastHit hit;
+        if (Physics.Raycast(raycastOrigin.position, Vector3.down, out hit, 0.2f)) // Ajusta la longitud del Raycast según tu jugador.
+        {
+            if (hit.collider.CompareTag("Ladrillo"))
+            {
+                if (!isBigMario)
+                {
+                    if (!collisionsWithCubes.ContainsKey(hit.collider.gameObject))
+                    {
+                        collisionsWithCubes[hit.collider.gameObject] = 0;
+                    }
+
+                    if (collisionsWithCubes[hit.collider.gameObject] < 3)
+                    {
+                        int randomScore = Random.Range(10, 51);
+                        ui.IncreaseScore(randomScore);
+                        collisionsWithCubes[hit.collider.gameObject]++;
+                    }
+                }
+                else
+                {
+                    ui.IncreaseScore(50);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Hongo"))
         {
             if (!isBigMario)
             {
-                // Aumenta el tamaño de Mario si no es grande.
-                transform.localScale *= bigMarioScale;
+                Vector3 newScale = transform.localScale;
+                newScale.y *= bigMarioScaleY;
+                transform.localScale = newScale;
                 isBigMario = true;
             }
 
-            Destroy(objeto.gameObject); // Destruye el hongo después de la colisión.
+            Destroy(hit.gameObject);
+            ui.IncreaseScore(60);
         }
     }
 }
